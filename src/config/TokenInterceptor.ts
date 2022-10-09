74ce4c394a051d30be54b4e64b74be794bef917f
轩小浅 <3102535247@qq.com>
2022-10-09 17:53:57 +0800

    修复

--- src/config/TokenInterceptor.ts
+++ src/config/TokenInterceptor.ts
@@ -2,8 +2,8 @@ import { Service, HandlerInterceptor } from 'xiao-koa'
 
 @Service()
 export default class TokenInterceptor implements HandlerInterceptor {
-  async preHandle(request: any, response: any, next: any) {
-    if (request.header['token']) {
+  async preHandle(ctx: any, next: any) {
+    if (ctx.request.header['token']) {
       await next()
     } else {
       throw { code: 403, error: '没有token' }
